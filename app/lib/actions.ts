6768a30696b1c0dfef8f6f491c35dde831f418ba
pdb3616109 <pdb3616109@users.noreply.github.com>
2026-01-17 01:02:11 +0800

    feat: add invoice editing functionality and update UI components

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import postgres from 'postgres'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
@@ -26,7 +28,37 @@ export async function createInvoice (formData: FormData) {
 
   console.log(rawFormData)
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})
   `
+  
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+export async function updateInvoice (id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+  `
+
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice (id: string) {
+
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
 }
