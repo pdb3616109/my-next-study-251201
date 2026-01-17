b6d795d6b8b55a31334f4ff727b241559b02fd4b
pdb3616109 <pdb3616109@users.noreply.github.com>
2026-01-17 20:47:04 +0800

    feat: add error handling and not found pages for invoice management

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,11 +27,19 @@ export async function createInvoice (formData: FormData) {
   const date = new Date().toISOString().split('T')[0]
 
   console.log(rawFormData)
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})
-  `
-  
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})
+    `
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error)
+    return {
+      message: 'Database Error: Failed to Create Invoice.'
+    }
+  }
+
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
@@ -46,18 +54,24 @@ export async function updateInvoice (id: string, formData: FormData) {
 
   const amountInCents = amount * 100
 
-  await sql`
-  UPDATE invoices
-  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-  WHERE id = ${id}
-  `
-
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error)
+    return { message: 'Database Error: Failed to Update Invoice.' }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice (id: string) {
+  throw new Error('Failed to Delete Invoice')
 
   await sql`DELETE FROM invoices WHERE id = ${id}`
   revalidatePath('/dashboard/invoices')
